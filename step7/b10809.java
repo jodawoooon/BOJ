668f1e0c6726fda6fbe69f9321d76e6f8ceb84ed
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-01-23 11:09:21 +0900

    2021-01-23 commit

---
+++ step7/b10809.java
@@ -0,0 +1,18 @@
+package boj.step7;
+
+import java.util.Scanner;
+
+public class b10809 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		
+		
+		
+		for (int i = 0; i < word.length(); i++) {
+			
+		}
+	}
+
+}
---
+++ step7/b11654.java
@@ -0,0 +1,11 @@
+package boj.step7;
+
+import java.util.Scanner;
+
+public class b11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ans = sc.next();
+		System.out.println(ans.charAt(0)-0);
+	}
+}
