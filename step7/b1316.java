a65e9adcc180dfa4e25abfcaf447cceb1b9058e9
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-01-28 21:52:39 +0900

    2021-01-28 commit ;; fail

---
+++ step7/b1316.java
@@ -0,0 +1,20 @@
+package boj.step7;
+
+import java.util.Scanner;
+
+public class b1316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] alp = new int[26];
+		int N = sc.nextInt();
+		for (int n = 0; n < N; n++) {
+			String word = sc.next();
+			for (int i = 0; i < word.length(); i++) {
+				alp[word.charAt(i)-'a']++;
+				if()
+			}
+		}
+	}
+
+}
