582903add7056f6ce56b61e1e8b8abd9e431d5be
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-02-04 22:04:37 +0900

    2021-02-04 hw : stack

--- workshop/b2493.java
+++ workshop/b2493.java
@@ -3,52 +3,60 @@ package boj.algo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
-//for문을 여러개 돌리니까 시간초과로 실패
+
 public class b2493 {
 	static int N;
 	static int[] top;
-	static int[] res;
+	static Stack<int[]> stack = new Stack<>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//왼쪽으로 레이저 동시에 발사. 가장 먼저 만나는 탑에서 신호 수신
 		//왼쪽 탑의 높이가 오른쪽 탑의 높이보다 높거나 같아야 수신 가능함.
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine()); //N = 탑의 수
 		
-		StringTokenizer st = new StringTokenizer(br.readLine()); 
-		top = new int[N+1];
-		res = new int[N+1];
-		for (int i = 0; i < N; i++) {
-			top[i] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+		top = new int[N+1]; // 탑의 높이를 저장할 배열 top
+		
+		for (int i = 1; i <= N; i++) {
+			top[i] = Integer.parseInt(st.nextToken()); //입력받아서 top 배열에 높이 저장
+			//System.out.println(top[i]);
 		}
 		
-		for (int i = N-1; i >= 0; i--) {
+		//첫 번째 탑은 레이저를 수신할 탑이 없으므로 항상 0이다. 따로 처리한다.
+		stack.push(new int[] {top[1], 1}); //stack에 첫 번째 top의 높이와 인덱스 정보를 저장한다
+		System.out.print(0); //인덱스 0 출력
+		
+		//System.out.println(stack.peek()[0]+" "+stack.peek()[1]+" "+stack.size());
+		
+		
+		for (int i = 2; i <= N; i++) {
 			
-			if(i==0) {
-				res[0] = 0;continue;
-			}
-			int tmp = i;
 			while(true) {
-				if(top[i]<=top[tmp-1]) {
-					res[i] = tmp;
+				
+				if(stack.size()==0) { 
+					//stack에서 모두 pop되면 -> 레이저를 수신하는 탑이 존재하지 않으면
+					stack.push(new int[] {top[i], i}); //자신의 값은 push하고
+					System.out.print(" "+0); //0을 출력하고 break
 					break;
-				}else {
-					tmp--;
 				}
 				
-				if(tmp==0){
-					res[i] = 0;
-					break;
+				if(stack.peek()[0] >= top[i]) {
+					//stack에서 peek한 것은 왼쪽 탑과 같다.
+					//왼쪽 탑의 높이가, 자신의 높이보다 같거나 높으면 레이저를 수신하므로
+					System.out.print(" "+stack.peek()[1]); // 왼쪽탑의 인덱스를 출력하고
+					stack.push(new int[] {top[i], i}); //자신의 값은 push한다.
+					break; //break;
+					
+				}else { //왼쪽탑의 높이가, 자신의 높이보다 작으면
+					stack.pop(); //pop하고 그 다음 탑의 값과 비교한다.
 				}
 			}
+			
+		}
 	
-		}
-		
-		for (int j = 0; j < N; j++) {
-			System.out.print(res[j]+ " ");
-		}
-		
 		
 	}
 }
